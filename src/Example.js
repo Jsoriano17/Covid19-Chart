6579e663b67cc007f955858b2e0ad9b7982eb61c
Jeffrey Soriano <jeffsor17@gmail.com>
2020-05-03 21:28:26 -0600

    responsive change

--- src/Example.js
+++ src/Example.js
@@ -8,6 +8,7 @@ import {
     CartesianGrid,
     Tooltip,
     Legend,
+    ResponsiveContainer,
 } from "recharts";
 const Example = ({ covid19Stats }) => {
     const formatData = (stats) => {
@@ -15,49 +16,53 @@ const Example = ({ covid19Stats }) => {
         stats.forEach(dp => {
             // see if country is in array
             // if(dp.country === 'US' || dp.country === 'China' || dp.country === 'United Kingdom' || dp.country === 'Canada' || dp.country === 'France' || dp.country === 'France') {
-            if(dp.confirmed > 20000) {
-            const index = newArray.findIndex((i) => i.name == dp.country);
-            if (index == -1) {
-                newArray.push({
-                    name: dp.country,
-                    deaths: dp.deaths,
-                    confirmed: dp.confirmed,
-                    recovered: dp.recovered,
-                })
+            if (dp.confirmed > 20000) {
+                const index = newArray.findIndex((i) => i.name == dp.country);
+                if (index == -1) {
+                    newArray.push({
+                        name: dp.country,
+                        deaths: dp.deaths,
+                        confirmed: dp.confirmed,
+                        recovered: dp.recovered,
+                    })
+                } else {
+                    newArray[index].deaths += dp.deaths
+                    newArray[index].confirmed += dp.confirmed
+                    newArray[index].recovered += dp.recovered
+                }
             } else {
-                newArray[index].deaths += dp.deaths
-                newArray[index].confirmed += dp.confirmed
-                newArray[index].recovered += dp.recovered
+                console.log('not working')
             }
-        } else {
-            console.log('not working')
-        }
         });
         console.log(newArray);
         return newArray
     };
     const data = formatData(covid19Stats);
     return (
-        <BarChart
-            width={1200}
-            height={700}
-            data={data}
-            margin={{
-                top: 5,
-                right: 50,
-                left: 10,
-                bottom: 5,
-            }}
-        >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" stroke="white"/>
-            <YAxis  stroke="white"/>
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="deaths" fill="#8884d8" />
-            <Bar dataKey="confirmed" fill="#82ca9d" />
-            <Bar dataKey="recovered" fill="#42ca9d" />
+        <>
+            <ResponsiveContainer width="100%" height={700}>
+                <BarChart
+                    width={1200}
+                    height={700}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 10,
+                        left: 10,
+                        bottom: 5,
+                    }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" stroke="white" />
+                <YAxis stroke="white" />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="deaths" fill="#8884d8" />
+                <Bar dataKey="confirmed" fill="#82ca9d" />
+                <Bar dataKey="recovered" fill="#42ca9d" />
         </BarChart>
+        </ResponsiveContainer>
+        </>
     );
 };
 export default Example;
